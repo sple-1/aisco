3102bc5ae36549715a7a5192ba40a2dcf2811382
syadzaarrana <syadzadesvianto@gmail.com>
2025-03-28 22:25:30 +0700

    feat: add base for paymentvariations

---
+++ modules/aisco.donation.banktransfer/aisco/donation/banktransfer/DonationImpl.java
@@ -0,0 +1,19 @@
+package aisco.donation.banktransfer;
+import aisco.donation.core.DonationComponent;
+import payment.page.core.*;
+
+public class DonationImpl extends DonationComponent {
+
+    public DonationImpl() {
+        System.out.println("\nDonation via Payment Gateway");
+    }
+
+    public void getDonation(){ 
+
+    }
+
+    public void addDonation(){
+        
+    }
+
+}
